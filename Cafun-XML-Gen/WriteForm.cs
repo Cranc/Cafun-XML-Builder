1e07d7390e5bf16e7faf342fe6942c89d3d582c3
Dennis Lindner <lindner_dennis@yahoo.de>
2016-01-13 01:28:01 +0100

    some work +added write function to write form

--- Cafun-XML-Gen/WriteForm.cs
+++ Cafun-XML-Gen/WriteForm.cs
@@ -18,12 +18,20 @@ namespace Cafun_XML_Gen
         }
 
         public String xml_description { get; set; }
+        public String name { get; set; }
+        public String author { get; set; }
 
         private void buttonWrite_Click(object sender, EventArgs e)
         {
             xml_description = "<description> \n";
-            xml_description += "< section caption = \"Guide\" >" + this.richTextBoxGuide.Text + "</section>";
+            xml_description += "<section caption = \"Guide\" >" + this.richTextBoxGuide.Text + "</section>";
+            xml_description += "<section caption=\"Description\">" + this.richTextBoxDescription.Text + "</section>";
+            xml_description += "</description>";
 
+            name = this.textBoxName.Text;
+            author = this.textBoxAuthor.Text;
+
+            DialogResult = DialogResult.OK;
         }
     }
 }
